e97458095aa7985c0298f517c5e6355befefae70
Yuan Li <tinaly1205@gmail.com>
2019-01-20 22:35:08 -0500

    add files

---
+++ build/precache-manifest.0e60873e2b7feae3e7958f94631e9f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotcontact/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "551601c385c535a87d14",
+    "url": "/robotcontact/static/js/runtime~main.551601c3.js"
+  },
+  {
+    "revision": "80dd62cb9791cddb35fb",
+    "url": "/robotcontact/static/js/main.80dd62cb.chunk.js"
+  },
+  {
+    "revision": "1f82549321afd24236b1",
+    "url": "/robotcontact/static/js/1.1f825493.chunk.js"
+  },
+  {
+    "revision": "80dd62cb9791cddb35fb",
+    "url": "/robotcontact/static/css/main.c55050e3.chunk.css"
+  },
+  {
+    "revision": "1f82549321afd24236b1",
+    "url": "/robotcontact/static/css/1.2b6f1e89.chunk.css"
+  },
+  {
+    "revision": "7293c1594b8a1327a3b895c83c6c937d",
+    "url": "/robotcontact/index.html"
+  }
+];
